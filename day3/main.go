40d314a7228c6dbdf17ff1733ab3d7a754916a36
jollyjerr <jollyjerr@gmail.com>
2021-12-06 08:12:33 -0700

    day three part two

--- day3/main.go
+++ day3/main.go
@@ -11,32 +11,94 @@ import (
 
 func main() {
 	data := loadData()
+	partOne(data)
+	partTwo(data)
+}
 
+type Score struct {
+	most  string
+	least string
+}
+
+func getMostCommon(data []string, stringindex int) Score {
+	count0 := 0
+	count1 := 0
+	for _, val := range data {
+		if string(val[stringindex]) == "1" {
+			count1++
+		} else {
+			count0++
+		}
+	}
+	if count1 >= count0 {
+		return Score{
+			most:  "1",
+			least: "0",
+		}
+	} else {
+		return Score{
+			most:  "0",
+			least: "1",
+		}
+	}
+}
+
+func partOne(data []string) {
 	var gamma []string
 	var epsilon []string
 	for i := 0; i < len(data[0]); i++ {
-		count1 := 0
-		count2 := 0
-		for j := 0; j < len(data); j++ {
-			if string(data[j][i]) == "1" {
-				count1++
-			} else {
-				count2++
-			}
-		}
-		if count1 > count2 {
-			gamma = append(gamma, "1")
-			epsilon = append(epsilon, "0")
-		} else {
-			gamma = append(gamma, "0")
-			epsilon = append(epsilon, "1")
-		}
+		score := getMostCommon(data, i)
+		gamma = append(gamma, score.most)
+		epsilon = append(epsilon, score.least)
 	}
 
 	gamdec, _ := strconv.ParseInt(strings.Join(gamma, ""), 2, 64)
 	epdec, _ := strconv.ParseInt(strings.Join(epsilon, ""), 2, 64)
 
 	fmt.Println(gamdec * epdec)
+}
+
+func partTwo(data []string) {
+	oxygen := make([]string, len(data))
+	copy(oxygen, data)
+
+	digitToCheck := 0
+	for len(oxygen) > 1 {
+		score := getMostCommon(oxygen, digitToCheck)
+
+		n := 0
+		for _, val := range oxygen {
+			if (string(val[digitToCheck])) == score.most {
+				oxygen[n] = val
+				n++
+			}
+		}
+		oxygen = oxygen[:n]
+		digitToCheck++
+	}
+
+	co2scrubber := make([]string, len(data))
+	copy(co2scrubber, data)
+
+	digitToCheck = 0
+	for len(co2scrubber) > 1 {
+		score := getMostCommon(co2scrubber, digitToCheck)
+
+		n := 0
+		for _, val := range co2scrubber {
+			if (string(val[digitToCheck])) == score.least {
+				co2scrubber[n] = val
+				n++
+			}
+		}
+		co2scrubber = co2scrubber[:n]
+		digitToCheck++
+	}
+
+	oxydec, _ := strconv.ParseInt(oxygen[0], 2, 64)
+	co2dec, _ := strconv.ParseInt(co2scrubber[0], 2, 64)
+
+	fmt.Println(oxydec * co2dec)
 }
 
 func loadData() []string {

489db70678f289931e34d3901b126aa9f7280fa3
Jeremiah Tabb <jollyjerr@gmail.com>
2022-12-06 08:17:24 -0700

    day 6

---
+++ 2022/src/day6.rs
@@ -0,0 +1,64 @@
+use crate::common::read_lines;
+use std::{
+    collections::{hash_map::HashMap, VecDeque},
+    path::Path,
+};
+
+pub fn day6<P: AsRef<Path>>(file_path: P) -> (usize, usize) {
+    let lines = read_lines(file_path);
+
+    let result = (find_first_marker(&lines, 4), find_first_marker(&lines, 14));
+    println!("{:?}", result);
+
+    result
+}
+
+fn find_first_marker(lines: &Vec<String>, marker_length: i32) -> usize {
+    let mut seen: HashMap<char, usize> = HashMap::new();
+    let mut window: VecDeque<char> = VecDeque::new();
+    let mut result: usize = 0;
+    for line in lines {
+        for (i, char) in line.chars().into_iter().enumerate() {
+            match seen.get(&char) {
+                Some(count) => {
+                    seen.insert(char, count + 1);
+                }
+                None => {
+                    seen.insert(char, 1);
+                }
+            }
+            window.push_back(char);
+
+            if window.len() >= marker_length.try_into().unwrap() {
+                if seen.values().all(|v| *v == 1 as usize) {
+                    result = i + 1;
+                    break;
+                } else {
+                    let to_remove = window.pop_front().unwrap();
+                    match seen.get(&to_remove) {
+                        None => panic!(),
+                        Some(count) => {
+                            if count > &1 {
+                                seen.insert(to_remove, count - 1);
+                            } else {
+                                seen.remove(&to_remove);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day6::day6;
+
+    #[test]
+    fn test_case() {
+        assert_eq!(day6("src/test_data/day6.txt"), (11, 26));
+    }
+}
--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -4,8 +4,9 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
-use crate::{day1::day1, day2::day2, day3::day3, day4::day4, day5::day5};
+use crate::{day1::day1, day2::day2, day3::day3, day4::day4, day5::day5, day6::day6};
 use std::env;
 
 fn main() {
@@ -26,8 +27,10 @@ fn main() {
         day3(file);
     } else if day_str == "4" {
         day4(file);
-    }  else if day_str == "5" {
+    } else if day_str == "5" {
         day5(file);
+    } else if day_str == "6" {
+        day6(file);
     }
 }
 

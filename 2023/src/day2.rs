b79115f62da336ca93b76a69f52dbad49a02e57b
Jeremiah Tabb <jollyjerr@gmail.com>
2023-12-02 08:48:35 -0700

    day2: part 2

--- 2023/src/day2.rs
+++ 2023/src/day2.rs
@@ -2,6 +2,10 @@ use crate::common::read_lines;
 use std::path::Path;
 
 pub fn day2<P: AsRef<Path>>(file_path: P, part_two: bool) -> u32 {
+    if part_two {
+        return solve_part_two(read_lines(file_path));
+    }
+
     let mut possible: Vec<u32> = Vec::new();
 
     for (index, line) in read_lines(file_path).iter().enumerate() {
@@ -23,6 +27,35 @@ pub fn day2<P: AsRef<Path>>(file_path: P, part_two: bool) -> u32 {
     }
 
     possible.iter().sum()
+}
+
+fn solve_part_two(lines: Vec<String>) -> u32 {
+    let mut powers: Vec<u32> = Vec::new();
+
+    for line in lines {
+        let rounds = get_rounds(&line);
+        let mut maxes: [u32; 3] = [0, 0, 0];
+
+        for round in rounds {
+            let red = get_val(&round, "red");
+            let green = get_val(&round, "green");
+            let blue = get_val(&round, "blue");
+
+            if red > maxes[0] {
+                maxes[0] = red;
+            }
+            if green > maxes[1] {
+                maxes[1] = green;
+            }
+            if blue > maxes[2] {
+                maxes[2] = blue;
+            }
+        }
+
+        powers.push(maxes[0] * maxes[1] * maxes[2]);
+    }
+
+    powers.iter().sum()
 }
 
 fn get_rounds(line: &str) -> Vec<Vec<&str>> {
@@ -54,6 +87,16 @@ mod tests {
 
     #[test]
     fn test_two() {
-        assert_eq!(day2("src/data/day2b.txt", false), 8);
+        assert_eq!(day2("src/data/day2b.txt", false), 2505);
+    }
+
+    #[test]
+    fn test_three() {
+        assert_eq!(day2("src/data/day2a.txt", true), 2286);
+    }
+
+    #[test]
+    fn test_four() {
+        assert_eq!(day2("src/data/day2b.txt", true), 70265);
     }
 }

26b0f9b2bb91db7f9bff25d0ae56073f70073a18
Jeremiah Tabb <jollyjerr@gmail.com>
2024-12-14 09:13:49 -0700

    wip: 2024 day 14 pt 2

--- 2024/day14.py
+++ 2024/day14.py
@@ -1,5 +1,7 @@
 import re
 import math
+import os
+import time
 
 
 def read_input(path):
@@ -18,8 +20,6 @@ def part_one(path, width, height, seconds):
     for (i, [px, py, vx, vy]) in enumerate(robots):
         col = (px + (vx * seconds)) % width
         row = (py + (vy * seconds)) % height
-        robots[i][0] = col
-        robots[i][1] = row
         if col != mid_col and row != mid_row:
             quadrant = (2 * (row >= mid_row)) + (col >= mid_col)
             quads[quadrant] += 1
@@ -29,3 +29,30 @@ def part_one(path, width, height, seconds):
 
 assert part_one('data/14.1.txt', 11, 7, 100) == 12
 print('part one:', part_one('data/14.2.txt', 101, 103, 100))
+
+
+def part_two(path, width, height):
+    robots = read_input(path)
+
+    counter = 0
+
+    # bruh is this for real...
+    while True:
+        if counter > 10000:
+            break
+        os.system('clear')
+        grid = [[0] * width for _ in range(height)]
+        for (i, [px, py, vx, vy]) in enumerate(robots):
+            grid[py][px] += 1
+            col = (px + vx) % width
+            row = (py + vy) % height
+            robots[i][0] = col
+            robots[i][1] = row
+        for line in grid:
+            print(''.join('*' if x > 0 else ' ' for x in line))
+        print(f'does this look like a tree!?!!?? lol ------ {counter}')
+        time.sleep(0.5)
+        counter += 1
+
+
+part_two('data/14.2.txt', 101, 103)

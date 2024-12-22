68c8c0b6e900a75d061f0f08e2a0ff7d760e7fff
Jeremiah Tabb <jollyjerr@gmail.com>
2024-12-21 20:22:28 -0700

    2024: day 21

--- 2024/day21.py
+++ 2024/day21.py
@@ -1,126 +1,61 @@
-from functools import cache
-
-
-numpad = {
-    # (0, 0): '#',
-    (0, 1): '0',
-    (0, 2): 'A',  # initial position
-    (1, 0): '1',
-    (1, 1): '2',
-    (1, 2): '3',
-    (2, 0): '4',
-    (2, 1): '5',
-    (2, 2): '6',
-    (3, 0): '7',
-    (3, 1): '8',
-    (3, 2): '9',
-}
-
-
-dirpad = {
-    # (0, 0): '#',
-    (0, 1): '^',
-    (0, 2): 'A',  # initial position
-    (-1, 0): '<',
-    (-1, 1): 'v',
-    (-1, 2): '>'
-}
-
-
-graphs = {
-    'numpad': numpad,
-    'dirpad': dirpad
-}
-
-
-DIRPAD = 'dirpad'
-NUMPAD = 'numpad'
-
-
-# <, ^, >, v
-directions = [(0, -1), (-1, 0), (0, 1), (1, 0)]
-
-
-@cache
-def rev_graph(graphname):
-    return {value: key for key, value in graphs[graphname].items()}
-
-
-@cache
-def neighbors(graphname, node):
-    graph = graphs[graphname]
-    (y, x) = node
-    options = [(y + dy, x + dx) for (dy, dx) in directions]
-    return [o for o in options if o in graph]
-
-
-@cache
-def all_paths(graphname, start, end):
-    out = []
-
-    def dfs(current, path):
-        path.append(current)
-        if current == end:
-            out.append(path[:])
-        else:
-            for n in neighbors(graphname, current):
-                if n not in path:
-                    dfs(n, path)
-        path.pop()
-
-    dfs(start, [])
-    return sorted(out, key=len)
-
-
-@cache
-def num_code_to_dir_codes(code):
-    rev = rev_graph(NUMPAD)
-    position = (0, 2)
-
-    options = set()
-
-    for char in list(code):
-        paths = all_paths(NUMPAD, position, rev[char])
-        if options:
-            for option in options:
-                options.remove(option)
-                for path in paths:
-                    options.add(option + path)
-        else:
-            for path in paths:
-                options.add(path)
-
-    return options
-
-
-@cache
-def shortest_path(code, graphname, start):
-    hparg = rev_graph(graphname)
-
-    position = start
-    full_path = []
-
-    for char in list(code):
-        full_path += all_paths(graphname, position, hparg[char])[0]
-        position = hparg[char]
-
-    return full_path
-
-
 def read_input(path):
     return [line.strip() for line in open(path, "r")]
 
 
+numpad = {'7': (0, 0), '8': (1, 0), '9': (2, 0), '4': (0, 1), '5': (1, 1), '6': (
+    2, 1), '1': (0, 2), '2': (1, 2), '3': (2, 2), ' ': (0, 3), '0': (1, 3), 'A': (2, 3)}
+dirpad = {' ': (0, 0), '^': (1, 0), 'A': (2, 0), '<': (0, 1), 'v': (1, 1), '>': (2, 1)}
+
+
+def fewest_presses(layer, target, user_moves):
+    return sum(user_moves[(layer, ki, kf)] for ki, kf in zip('A' + target, target))
+
+
+def solve(code, num_keyboards):
+    user_moves = {(0, ki, kf): 1 for ki in dirpad for kf in dirpad}
+
+    for layer in range(1, num_keyboards + 1):
+        board = numpad if layer == num_keyboards else dirpad
+
+        # fill in DP table of fewest presses by layer
+        for ki, (xi, yi) in board.items():
+            for kf, (xf, yf) in board.items():
+                lr = ('>' if xf > xi else '<') * abs(xf - xi)
+                ud = ('^' if yf < yi else 'v') * abs(yf - yi)
+
+                lr_first = float('inf')
+                if (xf, yi) != board[' ']:
+                    lr_first = fewest_presses(layer - 1, lr + ud + 'A', user_moves)
+
+                ud_first = float('inf')
+                if (xi, yf) != board[' ']:
+                    ud_first = fewest_presses(layer - 1, ud + lr + 'A', user_moves)
+
+                user_moves[(layer, ki, kf)] = min(lr_first, ud_first)
+
+    return fewest_presses(layer, code, user_moves)
+
+
 def part_one(path):
     codes = read_input(path)
+    out = 0
 
-    # robots = [(0, 2), (0, 2), (0, 2)]
+    for code in codes:
+        out += solve(code, 3) * int(code[:-1])
 
-    example = codes[0]
-    print(example)
-    print(num_code_to_dir_codes(example))
+    return out
 
-    return 0
+
+def part_two(path):
+    codes = read_input(path)
+    out = 0
+
+    for code in codes:
+        out += solve(code, 26) * int(code[:-1])
+
+    return out
 
 
 assert part_one('data/21.1.txt') == 126384
+print('part one:', part_one('data/21.2.txt'))
+print('part two:', part_two('data/21.2.txt'))

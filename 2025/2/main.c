710c358add2ab68ac685877bce556bd6e2076d23
Jeremiah Tabb <jollyjerr@gmail.com>
2025-12-01 23:20:59 -0700

    2025: day 2 pt 2

--- 2025/2/main.c
+++ 2025/2/main.c
@@ -1,30 +1,68 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
-bool is_valid(long long value) {
-    char strBuffer[30];
-    sprintf(strBuffer, "%lld", value);
-    int n = strlen(strBuffer);
+bool is_valid_pt_one(long long value) {
+  char strBuffer[30];
+  sprintf(strBuffer, "%lld", value);
+  int n = strlen(strBuffer);
 
-    if (n % 2 != 0) {
-        // all odd length numbers are valid I think?
-        return true;
+  // all odd length numbers are valid
+  if (n % 2 != 0) {
+    return true;
+  }
+
+  int tail = 0;
+  int head = n / 2;
+
+  for (int i = 0; i < n / 2; i++) {
+    if (strBuffer[tail] != strBuffer[head]) {
+      return true;
+    }
+    tail++;
+    head++;
+  }
+
+  return false;
+}
+
+bool is_valid_pt_two(long long value) {
+  char strBuffer[30];
+  sprintf(strBuffer, "%lld", value);
+  int n = strlen(strBuffer);
+
+  char sub_str[30];
+
+  // n - 1 so we only check substrings
+  for (int i = 0; i < n - 1; i++) {
+    // cannot be pattern mathematically
+    if (n % (i + 1) != 0) {
+      continue;
     }
 
-    int tail = 0;
-    int head = n / 2;
+    int sub_n = i + 1;
 
-    for (int i = 0; i < n / 2; i++) {
-        if (strBuffer[tail] != strBuffer[head]) {
-            return true;
-        }
-        tail++;
-        head++;
+    // get substring
+    strncpy(sub_str, strBuffer, sub_n);
+    sub_str[i + 1] = '\0';
+
+    // repeat substring until length of original string
+    char check[n + 1];
+    check[0] = '\0';
+
+    int loops = n / sub_n;
+    for (int j = 0; j < loops; j++) {
+      strcat(check, sub_str);
     }
 
-    return false;
+    // check if identical
+    if (strcmp(check, strBuffer) == 0) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 int main(int argc, char *argv[]) {
@@ -41,22 +79,28 @@ int main(int argc, char *argv[]) {
 
   long long start;
   long long end;
-  long long out;
+
+  long long pt_one = 0;
+  long long pt_two = 0;
 
   char *id_range = strtok(lineBuffer, ",");
   while (id_range != NULL) {
     if (sscanf(id_range, "%lld-%lld", &start, &end) == 2) {
-        for (long long i = start; i <= end; i++) {
-            if (!is_valid(i)) {
-                out += i;
-            }
+      for (long long i = start; i <= end; i++) {
+        if (!is_valid_pt_one(i)) {
+          pt_one += i;
         }
+        if (!is_valid_pt_two(i)) {
+          pt_two += i;
+        }
+      }
     }
 
     id_range = strtok(NULL, ",");
   }
 
-  printf("pt one: %lld\n", out);
+  printf("pt one: %lld\n", pt_one);
+  printf("pt two: %lld\n", pt_two);
 
   fclose(file);
   return EXIT_SUCCESS;

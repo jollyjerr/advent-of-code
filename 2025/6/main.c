ddf3cfbfba41eeb5eb21ebb69ac9cd629ea57618
Jeremiah Tabb <jollyjerr@gmail.com>
2025-12-06 00:17:06 -0700

    Refactor pt 1

--- 2025/6/main.c
+++ 2025/6/main.c
@@ -4,138 +4,108 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-  FILE *file = fopen(argv[1], "r");
+char *read_file(const char *filename) {
+  FILE *file = fopen(filename, "r");
+
   if (file == NULL) {
     perror("read data failed");
+    return NULL;
+  }
+
+  fseek(file, 0, SEEK_END);
+  long file_size = ftell(file);
+  rewind(file);
+
+  char *contents = (char *)malloc(file_size + 1);
+  fread(contents, 1, file_size, file);
+
+  contents[file_size] = '\0';
+
+  fclose(file);
+  return contents;
+}
+
+unsigned long long solve_pt_one(char *worksheet) {
+  unsigned long long out = 0;
+
+  int problems[1024][1024];
+  int problem_idxs[1024];
+
+  char number_buffer[1024] = "";
+  size_t number_buffer_len = 0;
+  bool skip_spaces = true;
+
+  size_t prob_idx = 0;
+  unsigned long prob_answer = 0;
+
+  for (int i = 0; i < strlen(worksheet); i++) {
+    int ch = worksheet[i];
+
+    bool end_of_line = ch == '\n';
+    bool end_of_number = (end_of_line || ch == ' ') && !skip_spaces;
+    bool end_of_problem = (ch == '+' || ch == '*');
+    bool is_int =
+        !end_of_line && !end_of_number && !end_of_problem && (ch != ' ');
+
+    if (end_of_number) {
+      int number_idx = problem_idxs[prob_idx];
+
+      problems[prob_idx][number_idx] = atoi(number_buffer);
+
+      problem_idxs[prob_idx]++;
+      prob_idx++;
+      number_buffer[0] = '\0';
+
+      skip_spaces = true;
+    }
+
+    if (end_of_line) {
+      prob_idx = 0;
+      skip_spaces = true;
+    }
+
+    if (end_of_problem) {
+      for (int i = 0; i < problem_idxs[prob_idx]; i++) {
+        if (prob_answer == 0) {
+          prob_answer = problems[prob_idx][i];
+        } else {
+          if (ch == '+') {
+            prob_answer += problems[prob_idx][i];
+          } else {
+            prob_answer *= problems[prob_idx][i];
+          }
+        }
+      }
+
+      out += prob_answer;
+      prob_answer = 0;
+
+      prob_idx++;
+    }
+
+    if (is_int) {
+      number_buffer_len = strlen(number_buffer);
+
+      number_buffer[number_buffer_len] = ch;
+      number_buffer[number_buffer_len + 1] = '\0';
+
+      skip_spaces = false;
+    }
+  }
+
+  return out;
+}
+
+int main(int argc, char *argv[]) {
+  char *worksheet = read_file(argv[1]);
+
+  if (worksheet == NULL) {
+    perror("no data");
     return EXIT_FAILURE;
   }
 
-  int num_problems = 0;
-  int problems[1024][1024];
-  int prob_num_lens[1024];
-  int prob_number_idxs[1024];
-  int prob_idx = 0;
-  bool skip_spaces = true;
+  printf("pt one: %llu\n", solve_pt_one(worksheet));
 
-  char number[1024] = "";
-  size_t num_len = 0;
-  unsigned long long local_result = 0;
-
-  unsigned long long pt_one = 0;
-
-  int ch;
-  while ((ch = fgetc(file)) != EOF) {
-    switch (ch) {
-    case '\n':
-      if (!skip_spaces) {
-        int number_idx = prob_number_idxs[prob_idx];
-
-        num_len = strlen(number);
-        if (num_len > prob_num_lens[prob_idx]) {
-          prob_num_lens[prob_idx] = num_len;
-        }
-
-        problems[prob_idx][number_idx] = atoi(number);
-
-        prob_number_idxs[prob_idx]++;
-        prob_idx++;
-        number[0] = '\0';
-
-        skip_spaces = true;
-      }
-      prob_idx = 0;
-      skip_spaces = true;
-      break;
-    case '+':
-      for (int i = 0; i < prob_number_idxs[prob_idx]; i++) {
-        if (local_result == 0) {
-          local_result = problems[prob_idx][i];
-        } else {
-          local_result += problems[prob_idx][i];
-        }
-      }
-
-      pt_one += local_result;
-      local_result = 0;
-
-      prob_idx++;
-      num_problems++;
-      break;
-    case '*':
-      for (int i = 0; i < prob_number_idxs[prob_idx]; i++) {
-        if (local_result == 0) {
-          local_result = problems[prob_idx][i];
-        } else {
-          local_result *= problems[prob_idx][i];
-        }
-      }
-
-      pt_one += local_result;
-      local_result = 0;
-
-      prob_idx++;
-      num_problems++;
-      break;
-    case ' ':
-      if (!skip_spaces) {
-        int number_idx = prob_number_idxs[prob_idx];
-
-        num_len = strlen(number);
-        if (num_len > prob_num_lens[prob_idx]) {
-          prob_num_lens[prob_idx] = num_len;
-        }
-
-        problems[prob_idx][number_idx] = atoi(number);
-
-        prob_number_idxs[prob_idx]++;
-        prob_idx++;
-        number[0] = '\0';
-
-        skip_spaces = true;
-      }
-      break;
-    default:
-      num_len = strlen(number);
-
-      number[num_len] = ch;
-      number[num_len + 1] = '\0';
-
-      skip_spaces = false;
-      break;
-    }
-  }
-
-  fclose(file);
-
-  printf("pt one: %llu\n", pt_one);
-
-  unsigned long long pt_two = 0;
-
-  char local_number[1024];
-
-  for (int i = 0; i < num_problems; i++) {
-    int num_len = prob_num_lens[i];
-    while (num_len > 0) {
-      for (int j = 0; j < prob_number_idxs[i]; j++) {
-        // big yikes...
-        int val = problems[i][j];
-        char str_val[1024];
-
-        sprintf(str_val, "%d", val);
-        printf("%c\n", str_val[num_len - 1]);
-
-        local_number[j] = str_val[num_len];
-      }
-
-      // printf("%d\n", atoi(local_number));
-
-      local_number[0] = '\0';
-      num_len--;
-    }
-  }
-
-  printf("pt two: %llu\n", pt_two);
-
+  free(worksheet);
   return EXIT_SUCCESS;
 }

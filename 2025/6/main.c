7e2dbc811a65916dba20be2ad6afeba6acfee60d
Jeremiah Tabb <jollyjerr@gmail.com>
2025-12-06 02:01:47 -0700

    2025: day 6 pt 2

--- 2025/6/main.c
+++ 2025/6/main.c
@@ -25,8 +25,8 @@ char *read_file(const char *filename) {
   return contents;
 }
 
-unsigned long long solve_pt_one(char *worksheet) {
-  unsigned long long out = 0;
+unsigned long solve_pt_one(char *worksheet) {
+  unsigned long out = 0;
 
   int problems[1024][1024];
   int problem_idxs[1024];
@@ -96,6 +96,85 @@ unsigned long long solve_pt_one(char *worksheet) {
   return out;
 }
 
+unsigned long long solve_pt_two(char *worksheet) {
+  unsigned long long out = 0;
+
+  int problem_count = 0;
+  char *lines[6];
+  size_t line_idx = 0;
+
+  char *line = strtok(worksheet, "\n");
+  while (line != NULL) {
+    lines[line_idx] = line;
+
+    line = strtok(NULL, "\n");
+    line_idx++;
+    problem_count++;
+  }
+
+  // read each column and save problem when meeting * or +
+  int problem_numbers[8192];
+  size_t prob_num_idx = 0;
+
+  // backwards each column
+  for (int i = strlen(lines[0]); i >= 0; i--) {
+    char local_number[1024] = {};
+    size_t local_number_idx = 0;
+
+    bool should_add = false;
+    bool should_mult = false;
+
+    // for each row
+    for (int j = 0; j < problem_count; j++) {
+      int ch = lines[j][i];
+
+      if (ch == ' ') {
+        continue;
+      }
+
+      if (ch == '+') {
+        should_add = true;
+        continue;
+      }
+
+      if (ch == '*') {
+        should_mult = true;
+        continue;
+      }
+
+      local_number[local_number_idx] = ch;
+      local_number_idx++;
+    }
+
+    if (strlen(local_number) > 0) {
+      problem_numbers[prob_num_idx] = atoi(local_number);
+      prob_num_idx++;
+    }
+
+    if (should_add || should_mult) {
+      unsigned long long solution = problem_numbers[0];
+
+      for (int k = 1; k < prob_num_idx; k++) {
+        if (should_add) {
+          solution += problem_numbers[k];
+        } else {
+          solution *= problem_numbers[k];
+        }
+      }
+
+      out += solution;
+
+      // clean memory for next problem
+      for (int k = 0; k < prob_num_idx; k++) {
+        problem_numbers[k] = 0;
+      }
+      prob_num_idx = 0;
+    }
+  }
+
+  return out;
+}
+
 int main(int argc, char *argv[]) {
   char *worksheet = read_file(argv[1]);
 
@@ -104,7 +183,8 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  printf("pt one: %llu\n", solve_pt_one(worksheet));
+  printf("pt one: %lu\n", solve_pt_one(worksheet));
+  printf("pt two: %llu\n", solve_pt_two(worksheet));
 
   free(worksheet);
   return EXIT_SUCCESS;

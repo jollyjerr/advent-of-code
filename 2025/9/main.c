1e72c237b1b1501cf56e08fb1dce91340509161a
Jeremiah Tabb <jollyjerr@gmail.com>
2025-12-09 21:27:08 -0700

    wip

--- 2025/9/main.c
+++ 2025/9/main.c
@@ -2,6 +2,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+  long long row1, col1, row2, col2, area;
+} Rec;
+
+bool valid_rec(Rec *rec, long long **points, size_t line_count) {
+  long long min_col = (rec->col1 < rec->col2) ? rec->col1 : rec->col2;
+  long long max_col = (rec->col1 > rec->col2) ? rec->col1 : rec->col2;
+  long long min_row = (rec->row1 < rec->row2) ? rec->row1 : rec->row2;
+  long long max_row = (rec->row1 > rec->row2) ? rec->row1 : rec->row2;
+
+  // check no red point is inside rectangle
+  for (size_t i = 0; i < line_count; i++) {
+    long long *check = points[i];
+    long long ccol = check[0];
+    long long crow = check[1];
+
+    bool point_inside_col = (ccol > min_col && ccol < max_col);
+    bool point_inside_row = (crow > min_row && crow < max_row);
+
+    if (point_inside_col && point_inside_row) {
+      return false;
+    }
+  }
+
+  // check no lines pass through rectangle
+  for (size_t i = 0; i < line_count - 1; i++) {
+    // next point is promised to be next in outline
+    long long *p1 = points[i];
+    long long *p2 = points[i + 1];
+
+    long long ccol1 = p1[0];
+    long long crow1 = p1[1];
+    long long ccol2 = p2[0];
+    long long crow2 = p2[1];
+
+    // horizontal lines
+    if (crow1 == crow2) {
+      long long r = crow1;
+      if (r > min_row && r < max_row) {
+        long long seg_min_col = (ccol1 < ccol2) ? ccol1 : ccol2;
+        long long seg_max_col = (ccol1 > ccol2) ? ccol1 : ccol2;
+
+        if (seg_max_col > min_col && seg_min_col < max_col) {
+          return false;
+        }
+      }
+      // vertical lines
+    } else if (ccol1 == ccol2) {
+      long long c = ccol1;
+      if (c > min_col && c < max_col) {
+        long long seg_min_row = (crow1 < crow2) ? crow1 : crow2;
+        long long seg_max_row = (crow1 > crow2) ? crow1 : crow2;
+
+        if (seg_max_row > min_row && seg_min_row < max_row) {
+          return false;
+        }
+      }
+    }
+  }
+
+  return true;
+}
+
 bool valid(long long *point, long long **points, size_t line_count) {
   // printf("point: %llu,%llu\n", point[0], point[1]);
   bool right_up = false;
@@ -73,6 +136,17 @@ bool valid(long long *point, long long **points, size_t line_count) {
   return false;
 }
 
+int compare_rec_desc(const void *a, const void *b) {
+  Rec *val_a = *(Rec **)a;
+  Rec *val_b = *(Rec **)b;
+
+  if (val_a->area < val_b->area)
+    return 1;
+  if (val_a->area > val_b->area)
+    return -1;
+  return 0;
+}
+
 int main(int argc, char *argv[]) {
   FILE *file = fopen(argv[1], "r");
 
@@ -115,38 +189,67 @@ int main(int argc, char *argv[]) {
 
   // ------------------ pt two ---------------
   long long pt_two = 0;
+
+  long long area_count = line_count * line_count;
+  Rec **recs = malloc(area_count * sizeof(Rec *));
+  size_t area_idx = 0;
+
+  for (size_t i = 0; i < area_count; i++) {
+    recs[i] = malloc(sizeof(Rec));
+  }
+
   for (size_t i = 0; i < line_count; i++) {
     for (size_t j = 0; j < line_count; j++) {
       long long *p1 = points[i];
       long long *p2 = points[j];
+      // this solution did not account for certain edge cases :(
+      // // same points or adjacent corners
+      // // do I need to include adjacent corners?
+      // if (p1[0] == p2[0] || p1[1] == p2[1]) {
+      //   continue;
+      // }
+      //
+      // long long p3[2] = {p1[0], p2[1]};
+      // long long p4[2] = {p2[0], p1[1]};
+      //
+      // // printf("rec: %llu,%llu %llu,%llu %llu,%llu %llu,%llu\n", p1[0],
+      // p1[1],
+      // //        p2[0], p2[1], *p3[0], *p3[1], *p4[0], *p4[1]);
+      //
+      // if (valid(p3, points, line_count) && valid(p4, points, line_count)) {
+      //   long long area =
+      //       llabs((p1[0] - p2[0]) + 1) * llabs((p1[1] - p2[1]) + 1);
+      //   if (area > pt_two) {
+      //     // printf("rec: %llu,%llu %llu,%llu %llu,%llu %llu,%llu\n", p1[0],
+      //     p1[1],
+      //     //        p2[0], p2[1], p3[0], p3[1], p4[0], p4[1]);
+      //     // printf("valid\n");
+      //     // printf("area: %llu\n", area);
+      //     pt_two = area;
+      //   }
+      // }
+      long long area = llabs((p1[0] - p2[0]) + 1) * llabs((p1[1] - p2[1]) + 1);
 
-      // same points or adjacent corners
-      // do I need to include adjacent corners?
-      if (p1[0] == p2[0] || p1[1] == p2[1]) {
-        continue;
-      }
+      recs[area_idx]->col1 = p1[0];
+      recs[area_idx]->row1 = p1[1];
+      recs[area_idx]->col2 = p2[0];
+      recs[area_idx]->row2 = p2[1];
+      recs[area_idx]->area = area;
 
-      long long p3[2] = {p1[0], p2[1]};
-      long long p4[2] = {p2[0], p1[1]};
-
-      // printf("rec: %llu,%llu %llu,%llu %llu,%llu %llu,%llu\n", p1[0], p1[1],
-      //        p2[0], p2[1], *p3[0], *p3[1], *p4[0], *p4[1]);
-
-      if (valid(p3, points, line_count) && valid(p4, points, line_count)) {
-        long long area =
-            llabs((p1[0] - p2[0]) + 1) * llabs((p1[1] - p2[1]) + 1);
-        if (area > pt_two) {
-          // printf("rec: %llu,%llu %llu,%llu %llu,%llu %llu,%llu\n", p1[0], p1[1],
-          //        p2[0], p2[1], p3[0], p3[1], p4[0], p4[1]);
-          // printf("valid\n");
-          // printf("area: %llu\n", area);
-          pt_two = area;
-        }
-      }
+      area_idx++;
     }
   }
 
-  // long long e1[2] = {83178,84037};
+  qsort(recs, area_count, sizeof(Rec *), compare_rec_desc);
+
+  for (size_t i = 0; i < area_count; i++) {
+    if (valid_rec(recs[i], points, line_count)) {
+      pt_two = recs[i]->area;
+      break;
+    }
+  }
+
+  // long long e1[2] = {83178, 84037};
   // long long e2[2] = {9, 1};
   // long long e3[2] = {7, 0};
   // long long e4[2] = {8, 1};
@@ -158,11 +261,18 @@ int main(int argc, char *argv[]) {
   // 4633892172 too high
   // 4633889290 too high
   // not 3781050, no feedback
+  // not 1396463530, no feedback
   printf("pt two: %llu\n", pt_two);
 
+  // ---------------- cleanup ---------------
+  for (size_t i = 0; i < area_count; i++) {
+    free(recs[i]);
+  }
+  free(recs);
   for (size_t i = 0; i < line_count; i++) {
     free(points[i]);
   }
   free(points);
+
   return EXIT_SUCCESS;
 }
